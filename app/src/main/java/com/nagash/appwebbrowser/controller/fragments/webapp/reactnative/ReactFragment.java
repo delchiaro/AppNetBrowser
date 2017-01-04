bfc7d3d32d7217e0607b8313d0f1986bb15e8859
Nagash <nagash91@gmail.com>
2017-01-04 20:37:57 +0100

    updated to reactNative 0.40, react 15.4.0-rc.4

--- app/src/main/java/com/nagash/appwebbrowser/controller/fragments/webapp/reactnative/ReactFragment.java
+++ app/src/main/java/com/nagash/appwebbrowser/controller/fragments/webapp/reactnative/ReactFragment.java
@@ -6,10 +6,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
-import com.facebook.react.LifecycleState;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactRootView;
+import com.facebook.react.common.LifecycleState;
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
 import com.facebook.react.shell.MainReactPackage;
 import com.nagash.appwebbrowser.controller.MainFragment;
